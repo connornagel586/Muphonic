eafdfc642e28d19d0d773209bdbf4ddad8550bd7
connornagel586 <connornagel@u.boisestate.edu>
2018-02-21 23:48:16 -0700

    More stuff

--- home_page.php
+++ home_page.php
@@ -11,12 +11,13 @@ session_start();
 		<link rel="stylesheet" type="text/css" href="style/main.css">
 	</head>
 	<header>
-		<h1>Muphonic</h1>	
+		<img src="images/mu.png" id="icon">
 	</header>
 	<body>
 		<nav>
 			<ul>
 				<a href="about.php">About</a>
+				<a href="home_page.php">Home</a>
 				<a href="chat_main.php">Chat</a>
 				<a href="forum_main.php">Forum</a>
 				<a href="index.php">Sign Out</a>
