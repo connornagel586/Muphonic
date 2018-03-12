2234bad110fd7103c61a95ecde47b4003a189f85
connornagel586 <connornagel@u.boisestate.edu>
2018-03-11 18:41:00 -0600

    Updated Database

--- muphonic.sql
+++ muphonic.sql
@@ -4,7 +4,6 @@ START TRANSACTION;
 SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `user_info`;
 CREATE TABLE IF NOT EXISTS `user_info` (
   `user_id` int(4) NOT NULL AUTO_INCREMENT,
   `username` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_mysql500_ci NOT NULL,
@@ -14,56 +13,56 @@ CREATE TABLE IF NOT EXISTS `user_info` (
   KEY `User ID` (`user_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
------------------------------------------------------------
+-- -----------------------------------------------------------
 
 DROP TABLE IF EXISTS `topics`;
 CREATE TABLE IF NOT EXISTS `topics` (
   `topic_id` int(4) NOT NULL AUTO_INCREMENT,
   `topic_title` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_mysql500_ci NOT NULL,
-  `created_by` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_mysql500_ci NOT NULL,
+  `created_by` int(4) NOT NULL,
   `num_comments` int(100) NOT NULL,
-  `time_created` datetime NOT NULL,
+  `time_created` DATETIME NOT NULL,
   PRIMARY KEY (`topic_id`),
-  FOREIGN KEY (`created_by`) REFERENCES user_info(`user_id`) ON UPDATE CASCADE,
-  KEY `Topic ID` (`topic_id`)
+  KEY `Topic ID` (`topic_id`),
+  CONSTRAINT `topicID_user` FOREIGN KEY (`created_by`) REFERENCES `user_info` (`user_id`) ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
------------------------------------------------------------
+-- -----------------------------------------------------------
 
 DROP TABLE IF EXISTS `comments`;
 CREATE TABLE IF NOT EXISTS `comments` (
   `comment_id` int(4) NOT NULL AUTO_INCREMENT,
   `topic_id` int(4) NOT NULL,
-  `posted_by` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_mysql500_ci NOT NULL,
+  `posted_by` int(4) NOT NULL,
   `email` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_mysql500_ci NOT NULL,
-  `time_posted` datetime NOT NULL,
+  `time_posted` DATETIME NOT NULL,
   PRIMARY KEY (`comment_id`),
-  FOREIGN KEY (`posted_by`) REFERENCES user_info(`user_id`) ON UPDATE CASCADE,
+  CONSTRAINT `comment_user` FOREIGN KEY (`posted_by`) REFERENCES `user_info` (`user_id`) ON UPDATE CASCADE,
   KEY `Comment ID` (`comment_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
------------------------------------------------------------
+-- -----------------------------------------------------------
 
 DROP TABLE IF EXISTS `rooms`;
-DROP TABLE IF NOT EXISTS `rooms`(
+CREATE TABLE IF NOT EXISTS `rooms`(
   `room_id` int(4) NOT NULL AUTO_INCREMENT,
   `room_title` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_mysql500_ci NOT NULL,
-  `created_by` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_mysql500_ci NOT NULL,
-  `time_created`datetime NOT NULL,
+  `created_by` int(4) NOT NULL,
+  `time_created`DATETIME NOT NULL,
   PRIMARY KEY (`room_id`),
-  FOREIGN KEY (`create_by`) REFERENCES user_info(`user_id`) ON UPDATE CASCADE,
+  CONSTRAINT `room_user` FOREIGN KEY (`created_by`) REFERENCES `user_info` (`user_id`) ON UPDATE CASCADE,
   KEY `Room ID` (`room_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
------------------------------------------------------------
+-- -----------------------------------------------------------
 
 DROP TABLE IF EXISTS `chat`;
-DROP TABLE IF NOT EXISTS `chat`(
+CREATE TABLE IF NOT EXISTS `chat`(
   `chat_id` int(4) NOT NULL AUTO_INCREMENT,
-  `posted_by` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_mysql500_ci NOT NULL,
-  `time_posted` datetime NOT NULL,
+  `posted_by` int(4) NOT NULL,
+  `time_posted` DATETIME NOT NULL,
   `message` varchar(300) CHARACTER SET utf8 COLLATE utf8_general_mysql500_ci NOT NULL,
   PRIMARY KEY (`chat_id`),
-  FOREIGN KEY (`posted_by`) REFERENCES user_info(`user_id`),
+  CONSTRAINT `chat_user` FOREIGN KEY (`posted_by`) REFERENCES `user_info` (`user_id`) ON UPDATE CASCADE,
   KEY `Chat ID` (`chat_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
